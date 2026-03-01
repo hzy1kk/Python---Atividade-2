dc5c8304cfca37b09170be1f3507b0dbf224a046
hzy1kk <lucaslohan@colegiopaulodetarso.com.br>
2026-03-01 15:37:21 +0000

    save2

--- imposto.py
+++ imposto.py
@@ -1,17 +1,16 @@
-preco = float(input("Preço do produto: "))
-imposto = 1.1
-print("Preço com imposto:", preco * imposto)
-
 try:
     texto = input("Preço do produto: ").strip().replace(",", ".")
     preco = float(texto)
 
     if preco < 0:
-        raise ValueError("Preço nffativo não permitido.")
+        raise ValueError("Preço negativo não permitido.")
 
     imposto = 1.1
     print("Preço com imposto:", preco * imposto)
-except ValueError:
-    print("Preço inválido. Digite um valor numérico maior ou igual a zero.")
+
+except ValueError as e:
+    # pega tanto erro de conversão quanto o nosso erro de negativo
+    print(f"Preço inválido: {e}. Digite um valor numérico maior ou igual a zero.")
+
 except EOFError:
     print("Nenhuma entrada foi fornecida.")
